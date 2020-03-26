8e55a4a71a3f073da360c39b12a927e8433fe30f
Mazharul Emon <emonmazharul@yandex.com>
2020-03-26 14:41:42 +0600

    fixing package json error

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const fastify = require('./fastify/app');
 
-const PORT = process.env.PORT || 5000
+const port = process.env.PORT || 5000
 async function start(){
 	try {
 
-		fastify.listen(PORT);
-		console.log(`server listen on ${PORT}`)
+		fastify.listen(port);
+		console.log(`server listen on ${port}`)
 	}
 	catch (e){
 		console.log(e)
