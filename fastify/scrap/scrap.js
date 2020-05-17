1cfada514fcfecac015be5e98015ba420739871a
Mazharul Emon <emonmazharul@yandex.com>
2020-05-18 00:14:42 +0600

    fixing a little bug

--- fastify/scrap/scrap.js
+++ fastify/scrap/scrap.js
@@ -115,7 +115,7 @@ async function scrapFromWorld(url,country) {
 	const keys = $(headers).children()
 
 
-	for(let i=0; i<tbody.length;i++){
+	for(let i=1; i<tbody.length;i++){
 		let targetChild = $(tbody[i]).children()[1];
 		let targetChildText = $(targetChild).text();
 		if(targetChildText===country){
